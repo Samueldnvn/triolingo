3bcb8212922369070ad7bc39998bc071cbe1fe8f
Samuel Donovan <samueldnvn4@gmail.com>
2026-03-15 14:17:38 -0400

    Use new filename cppCombined_v2.js to bypass caching
    - Created copy as cppCombined_v2.js
    - Valid JSON with 3 units, 18 lessons
    - Updated index.html to use new file
    - New cache buster to force reload

---
+++ triolingo/questions/cppCombined_v2.js
@@ -0,0 +1,171 @@
+// CPP - Combined Course
+// C++ for Python Programmers + Algorithms
+
+window.cppCombined = {
+      "courseName": "C++",
+      "units": [
+            {
+                  "unitId": "1",
+                  "unitName": "1. C++ Environment and Language Basics",
+                  "lessons": [
+                        {
+                              "id": "1-1",
+                              "title": "Introduction to C++",
+                              "xp": 200,
+                              "type": "lesson",
+                              "difficulty": "beginner",
+                              "questions": []
+                        },
+                        {
+                              "id": "1-2",
+                              "title": "Variables and Data Types",
+                              "xp": 220,
+                              "type": "lesson",
+                              "difficulty": "beginner",
+                              "questions": []
+                        },
+                        {
+                              "id": "1-3",
+                              "title": "Input and Output",
+                              "xp": 220,
+                              "type": "lesson",
+                              "difficulty": "beginner",
+                              "questions": []
+                        },
+                        {
+                              "id": "1-4",
+                              "title": "Basic Operators",
+                              "xp": 220,
+                              "type": "lesson",
+                              "difficulty": "beginner",
+                              "questions": []
+                        },
+                        {
+                              "id": "1-5",
+                              "title": "Control Flow: If Statements",
+                              "xp": 220,
+                              "type": "lesson",
+                              "difficulty": "beginner",
+                              "questions": []
+                        },
+                        {
+                              "id": "1-6",
+                              "title": "Control Flow: Loops",
+                              "xp": 220,
+                              "type": "lesson",
+                              "difficulty": "beginner",
+                              "questions": []
+                        }
+                  ]
+            },
+            {
+                  "unitId": "2",
+                  "unitName": "2. Functions and Data Structures",
+                  "lessons": [
+                        {
+                              "id": "2-1",
+                              "title": "Functions and Scope",
+                              "xp": 85,
+                              "type": "lesson",
+                              "difficulty": "intermediate",
+                              "questions": []
+                        },
+                        {
+                              "id": "2-2",
+                              "title": "Arrays and Vectors",
+                              "xp": 85,
+                              "type": "lesson",
+                              "difficulty": "intermediate",
+                              "questions": []
+                        },
+                        {
+                              "id": "2-3",
+                              "title": "Strings and String Manipulation",
+                              "xp": 85,
+                              "type": "lesson",
+                              "difficulty": "intermediate",
+                              "questions": []
+                        },
+                        {
+                              "id": "2-4",
+                              "title": "References and Pointers",
+                              "xp": 85,
+                              "type": "lesson",
+                              "difficulty": "intermediate",
+                              "questions": []
+                        },
+                        {
+                              "id": "2-5",
+                              "title": "Basic Algorithms",
+                              "xp": 85,
+                              "type": "lesson",
+                              "difficulty": "intermediate",
+                              "questions": []
+                        },
+                        {
+                              "id": "2-6",
+                              "title": "Advanced Control Flow",
+                              "xp": 85,
+                              "type": "lesson",
+                              "difficulty": "intermediate",
+                              "questions": []
+                        }
+                  ]
+            },
+            {
+                  "unitId": "3",
+                  "unitName": "3. Object-Oriented Programming",
+                  "lessons": [
+                        {
+                              "id": "3-1",
+                              "title": "Classes and Objects",
+                              "xp": 85,
+                              "type": "lesson",
+                              "difficulty": "intermediate",
+                              "questions": []
+                        },
+                        {
+                              "id": "3-2",
+                              "title": "Methods and Access Control",
+                              "xp": 85,
+                              "type": "lesson",
+                              "difficulty": "intermediate",
+                              "questions": []
+                        },
+                        {
+                              "id": "3-3",
+                              "title": "Inheritance",
+                              "xp": 85,
+                              "type": "lesson",
+                              "difficulty": "intermediate",
+                              "questions": []
+                        },
+                        {
+                              "id": "3-4",
+                              "title": "Polymorphism",
+                              "xp": 85,
+                              "type": "lesson",
+                              "difficulty": "intermediate",
+                              "questions": []
+                        },
+                        {
+                              "id": "3-5",
+                              "title": "Operator Overloading",
+                              "xp": 85,
+                              "type": "lesson",
+                              "difficulty": "intermediate",
+                              "questions": []
+                        },
+                        {
+                              "id": "3-6",
+                              "title": "Advanced OOP Concepts",
+                              "xp": 85,
+                              "type": "lesson",
+                              "difficulty": "intermediate",
+                              "questions": []
+                        }
+                  ]
+            }
+      ]
+};
+ 
